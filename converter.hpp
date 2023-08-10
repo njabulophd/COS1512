5bfabfc96daa6e0190d50950b9c57a671d2a9c92
njabulo <njabulophd@gmail.com>
2023-08-10 17:05:24 +0200

    This file will hold a function/namespcae for crea- ting a custom format to store our module data for 'q2.cpp'

---
+++ converter.hpp
@@ -0,0 +1,5 @@
+
+This is a header file which will be used to create a namespace or maybe a function to convert/store a .cpp file into a custom .txt file 
+#ifndef HEADER_HPP
+#define HEADER_HPP
+#endif
