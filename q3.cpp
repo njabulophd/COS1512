de648b5dca6b2523ca44c073a70360c50661a581
njabulo <njabulophd@gmail.com>
2023-08-10 17:00:26 +0200

    Created a 'q3.cpp' which will take the contents of 'q2.cpp' and save them in a custom extension/format using fstream

---
+++ q3.cpp
@@ -0,0 +1,16 @@
+#include "q2.cpp"
+#include <fstream>
+#include <string.h>
+//Input file from Q2
+string file()
+{
+	ofstream file(string name);
+	return name;
+}
+
+
+
+int main()
+{
+	return 0;
+}
