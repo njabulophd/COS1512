4c2d400f4108483c60620e35e368ea22894c4abb
njabulo <njabulophd@gmail.com>
2023-08-11 14:32:12 +0200

    modified the file so that most errors are resolved needed to add a '[i]' between 'exams.' and 'date' and also betweeen 'exams.' and 'time' also betweeen 'exams.' and 'module'. We still have a unresolved linking error pertaining to our custom class constructor or maybe even the specified 'default' constructor

--- q4.cpp
+++ q4.cpp
@@ -16,8 +16,8 @@ int main()
 {
     ExamType exams[12];
     for (int i = 0; i < 12; i++)
-        if (exams.module[i] == "COS1512")
-            cout << "COS1512 will be written on " << exams.date << " at " << exams.time;
+        if (exams[i].module == "COS1512")
+            cout << "COS1512 will be written on " << exams[i].date << " at " << exams[i].time;
     return 0;
 }
 
