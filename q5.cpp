4ae3bf8862f057c5e8e988a9ad4889528b7fe7a9
njabulo <njabulophd@gmail.com>
2023-08-12 15:45:27 +0200

    Added a c++ file 'q5.cpp' which has a default date constructor which will set a default date. And also be able to update the date to whatever the user sets it to be.

---
+++ q5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Date{
+public:
+    //constructors
+    Date();
+    Date(int day, int month, int year)
+    {
+        day = 14;
+        month = 09;
+        year = 1752;
+
+    }
+    //accessors
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+    //mutators
+    void setDay(int day);
+    void setMonth(int month);
+    void setYear(int year);
+    //operators to calculate next and previous days
+    Date &operator++();
+    Date &operator--();
+    bool operator<(const Date &d);
+private:
+    //the current day month and year
+    int theday;
+    int themonth;
+    int theyear;
+    //return the length of current month, taking into
+    //account leap years
+    int monthLength();
+};
+
+
+int main()
+{
+    return 0;
+}
